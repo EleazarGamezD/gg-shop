3b1672348954476e53f17c3c331f21fe1734e955
ELEAZAR GAMEZ <82742317+froitzent@users.noreply.github.com>
2023-03-09 21:09:38 -0500

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+@Module({
+  imports: [
+  ConfigModule.forRoot(), // para leer archivo .env
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    autoLoadEntities: true,
+    synchronize: true,  //en produccion no se activa
+
+  })
+  ],
+  
+})
+export class AppModule {}
