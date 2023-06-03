0d4eaa0b5a736f87f09ec4d530cb78295dac1cf5
ELEAZAR GAMEZ <82742317+froitzent@users.noreply.github.com>
2023-06-02 23:26:17 -0500

    index

--- src/app.module.ts
+++ src/app.module.ts
@@ -36,7 +36,7 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
    SeedModule, 
    FilesModule,
    ServeStaticModule.forRoot({
- rootPath: join(__dirname,'..','/public'),
+ rootPath: join(__dirname,'..','public'),
 }),
    AuthModule,
    MessagesWsModule
